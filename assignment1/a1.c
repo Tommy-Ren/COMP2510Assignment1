cab213efd49f4d3428ddbe6f85b2a04777db7ee1
Tommy Ren <tren7@my.bcit.ca>
2024-02-03 19:35:45 -0800

    works?

--- assignment1/a1.c
+++ assignment1/a1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     printf("123");
     printf("NAM ANH");
     printf("It Worked");
+    printf("Tommy");
+    return 0;
 }
